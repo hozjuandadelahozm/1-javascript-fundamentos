61e9e0195014173097deadd6f961e6a97d94822e
Juan David De la hoz <hozjuanda11@gmail.com>
2022-03-01 22:56:29 -0500

    segundo commit

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,11 @@
+
+var π = Math.PI;
+
+let _objeto$123 = 123;
+let precio99_99 = 123;
+
+let jugadorConPuntajeMasAlto = 'Juan David'; // Lower camel case para los nombres de variables
+
+class JuegoAnio {   // Upper camel case para los nombres de clases
+
+}
